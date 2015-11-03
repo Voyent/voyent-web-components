8ff62611cd1874e9e3f140443838b3ec8db2be76
cgug <carlo.guglielmin@icesoft.com>
2015-11-03 11:01:02 -0700

    Update

--- components/bridgeit-web-components/bridgeit-event-monitor/bridgeit-event-monitor.js
+++ components/bridgeit-web-components/bridgeit-event-monitor/bridgeit-event-monitor.js
@@ -2,6 +2,66 @@ Polymer({
 
     is: "bridgeit-event-monitor",
     
+    properties: {
+        /**
+         * Defines the width of the underlying SVG element
+         * @default 100%
+         */
+        width: { type: String, value: "100%" },
+        /**
+         * Defines the height of the underlying SVG element
+         * @default 100px
+         */
+        height: { type: String, value: "100px" },
+        /**
+         * Defines the left padding of the graph inside the SVG element
+         * @default 20
+         */
+        padding: { type: Number, value: 20 },
+        /**
+         * Defines the circle radius for all events show on the graph
+         * @default 10
+         */
+        circleradius: { type: Number, value: 10 },
+        /**
+         * Defines the default color to use for events coming from an unknown service
+         * @default orange
+         */
+        defaultcolor: { type: String, value: "orange" },
+        /**
+         * Defines whether to show the graph Legend or not
+         * @default true
+         */
+        uselegend: { type: String, value: "true" },
+        /**
+         * Defines whether to enable zoom in/out functionality (via mousewheel) for the graph
+         * @default true
+         */
+        usezoom: { type: String, value: "true" },
+        /**
+         * Defines whether to enable drag/panning functionality for the graph
+         * @default true
+         */
+        usedrag: { type: String, value: "true" },
+        /**
+         * Defines whether events show on the graph should be clickable or not
+         * If clickable the details of the event will be shown as part of this component
+         * @default true
+         */
+        useclickable: { type: String, value: "true" },
+        /**
+         * Defines whether to enable mouseover features of event circles
+         * When enabled mousing over a circle will bring it to the front, temporarily increase the size, and border it
+         * @default true
+         */
+        usemouseover: { type: String, value: "true" },
+        /**
+         * Defines whether the graph should be dynamically and responsively resized as the browser width/height changes
+         * @default true
+         */
+        useresize: { type: String, value: "true" },
+    },
+    
     /**
      * Display a marble style graph based on the passed metric data
      * @param data array of the metric events to show on the graph
@@ -9,15 +69,16 @@ Polymer({
     show: function(data) {
         var _this = this;
         var vis = d3.select("#eventmonitorsvg");
-        var PADDING = 20,
-            CIRCLE_RADIUS = 10;
-            WIDTH = parseInt(vis.style("width"))-PADDING,
-            HEIGHT = parseInt(vis.style("height")),
-            DEFAULT_COLOR = "orange";
-            
+        
+        // Set our basic width/height and then calculate the result without units
+        vis.attr("width", this.width);
+        vis.attr("height", this.height);
+        var calcWidth = parseInt(vis.style("width"))-this.padding;
+        var calcHeight = parseInt(vis.style("height"));
+        
         // Sizing minimum
-        if (WIDTH < 100 && window.innerWidth && window.innerWidth > 500) {
-            WIDTH = window.innerWidth - 400;
+        if (calcWidth < 100 && window.innerWidth && window.innerWidth > 500) {
+            calcWidth = window.innerWidth - 400;
         }
             
         // Clear our old graph first and reset the cursor
@@ -26,16 +87,18 @@ Polymer({
         
         // Before progressing check if we even have data to graph
         // If we don't we'll disable zoom/drag and show an error message
-        if (typeof data == "undefined" || data == null || data.length == 0) {
+        if (typeof data === "undefined" || data === null || data.length === 0) {
             console.log("No data was passed to event monitor graph.");
+            
+            this.customTitle = null;
             
             vis.call(d3.behavior.zoom().on("zoom", null));
             vis.call(d3.behavior.drag().on("drag", null));
             
             vis.append("svg:g").attr("class", "axis");
             vis.append("text").text("No event data was found, please try again.")
-                .attr("x", PADDING)
-                .attr("y", HEIGHT/2)
+                .attr("x", this.padding)
+                .attr("y", calcHeight/2)
                 .attr("fill", "red");
             
             return;
@@ -61,7 +124,7 @@ Polymer({
         for (i = 0; i < data.length; i++) {
             addCurrent = false;
             // Add the first service automatically
-            if (services.length == 0) {
+            if (services.length === 0) {
                 addCurrent = true;
             }
             // Otherwise check the existing services list against our data object
@@ -86,7 +149,7 @@ Polymer({
             return new Date(d.time);
         }), d3.max(data, function(d) {
             return new Date(d.time);
-        })]).range([PADDING, WIDTH-PADDING]);
+        })]).range([this.padding, calcWidth-this.padding]);
         
         // Use the scale to make an axis
         xAxis = d3.svg.axis().scale(xScale)
@@ -96,145 +159,172 @@ Polymer({
         // Vertically center the graph
         vis.append("svg:g")
             .attr("class", "axis")
-            .attr("transform", "translate(0," + (HEIGHT/2) + ")")
+            .attr("transform", "translate(0," + (calcHeight/2) + ")")
             .call(xAxis);
             
         // Add zoom functionality
-        vis.call(d3.behavior.zoom()
-            .x(xScale)
-            .on("zoom", function() {
-                vis.select("g.axis").call(xAxis);
-                vis.selectAll("circle")
-                    .attr("cx", function(d) { return xScale(new Date(d.time)) });
-            })
-            .on("zoomstart", function() {
-                vis.style("cursor", "zoom-in");
-            })
-            .on("zoomend", function() {
-                vis.style("cursor", null);
-            })
-        );
+        if (this.usezoom == 'true') {
+            vis.call(d3.behavior.zoom()
+                .x(xScale)
+                .on("zoom", function() {
+                    vis.select("g.axis").call(xAxis);
+                    vis.selectAll("circle")
+                        .attr("cx", function(d) { return xScale(new Date(d.time)); });
+                })
+                .on("zoomstart", function() {
+                    vis.style("cursor", "zoom-in");
+                })
+                .on("zoomend", function() {
+                    vis.style("cursor", null);
+                })
+            );
+        }
+        else {
+            vis.call(d3.behavior.zoom().on("zoom", null));
+            vis.call(d3.behavior.zoom().on("zoomstart", null));
+            vis.call(d3.behavior.zoom().on("zoomend", null));
+        }
         
-        // Customize our drag functionality
-        vis.call(d3.behavior.drag()
-            .on("dragstart", function() {
-                vis.style("cursor", "move");
-                d3.select("body").style("cursor", "move");
-            })
-            .on("dragend", function() {
-                vis.style("cursor", null);
-                d3.select("body").style("cursor", null);                   
-            })
-        );
+        if (this.usedrag == 'true') {
+            // Customize our drag functionality
+            vis.call(d3.behavior.drag()
+                .on("dragstart", function() {
+                    vis.style("cursor", "move");
+                    d3.select("body").style("cursor", "move");
+                })
+                .on("dragend", function() {
+                    vis.style("cursor", null);
+                    d3.select("body").style("cursor", null);                   
+                })
+            );
+        }
+        else {
+            vis.call(d3.behavior.drag().on("drag", null));
+            vis.call(d3.behavior.drag().on("dragstart", null));
+            vis.call(d3.behavior.drag().on("dragend", null));
+        }
         
         // Draw a circle for every piece of data
         var clickedCircle; // Used to track SVG object that was clicked
         vis.selectAll("circle.line")
             .data(data)
             .enter().append("circle")
-            .attr("cy", HEIGHT/2)
-            .attr("cx", function(d) { return xScale(new Date(d.time)) })
-            .attr("r", CIRCLE_RADIUS)
+            .attr("cy", calcHeight/2)
+            .attr("cx", function(d) { return xScale(new Date(d.time)); })
+            .attr("r", this.circleradius)
             .attr("stroke", "black")
             .attr("stroke-width", 1)
-            .attr("cursor", "pointer")
+            .attr("cursor", function() { return (_this.useclickable == 'true') ? "pointer" : null; })
             .attr("title", function (d) { return new Date(d.time); })
             .style("fill", function(d) {
                 var toReturn = colors[d.service];
                 if (!toReturn) {
-                    return DEFAULT_COLOR;
+                    return _this.defaultcolor;
                 }
                 return toReturn; 
             })
             .on("click", function(d, i) {
-                // First we restore any previously clicked circle to the normal radius
-                if (_this.clickedCircle) {
-                    var old = d3.select(_this.clickedCircle);
-                    old.attr("r", CIRCLE_RADIUS);
-                    old.classed("clickedCircle", false);
-                }
+                if (_this.useclickable == 'true') {
+                    // First we restore any previously clicked circle to the normal radius
+                    if (_this.clickedCircle) {
+                        var old = d3.select(_this.clickedCircle);
+                        old.attr("r", _this.circleradius);
+                        old.classed("clickedCircle", false);
+                    }
+                        
+                    // Check if we're re-clicking the same circle, in which case we want to hide the details
+                    // This will basically function as a toggle
+                    if (d == _this.clickedData) {
+                        _this.clickedData = null;
+                        _this.clickedDataFormatted = null;
+                        document.getElementById('eventDetails').style.display = "none";
+                        
+                        return;
+                    }
                     
-                // Check if we're re-clicking the same circle, in which case we want to hide the details
-                // This will basically function as a toggle
-                if (d == _this.clickedData) {
-                    _this.clickedData = null;
-                    _this.clickedDataFormatted = null;
-                    document.getElementById('eventDetails').style.display = "none";
-                    
-                    return;
+                    // Otherwise set our data object for display on the page and show the details
+                    var sel = d3.select(this);
+                    sel.attr("r", _this.circleradius*2);
+                    sel.classed("clickedCircle", true);
+                    _this.clickedCircle = this;
+                    _this.clickedData = d;
+                    _this.clickedDataFormatted = JSON.stringify(d.data);
+                    document.getElementById('eventDetails').style.display = "inline";
                 }
-                
-                // Otherwise set our data object for display on the page and show the details
-                var sel = d3.select(this);
-                sel.attr("r", CIRCLE_RADIUS*2);
-                sel.classed("clickedCircle", true);
-                _this.clickedCircle = this;
-                _this.clickedData = d;
-                _this.clickedDataFormatted = JSON.stringify(d.data);
-                document.getElementById('eventDetails').style.display = "inline";
             })
             .on("mouseover", function(d, i) {
-                var sel = d3.select(this);
-                // Only do mouseover radius functionality if we're not already clicked
-                if (!sel.classed("clickedCircle")) {
-                    sel.attr("r", CIRCLE_RADIUS+3);
+                if (_this.usemouseover == 'true') {
+                    var sel = d3.select(this);
+                    // Only do mouseover radius functionality if we're not already clicked
+                    if (!sel.classed("clickedCircle")) {
+                        sel.attr("r", _this.circleradius+3);
+                    }
+                    // Always change stroke width and bring to the front though
+                    sel.transition().attr("stroke-width", 2);
+                    this.parentElement.appendChild(this);
                 }
-                // Always change stroke width and bring to the front though
-                sel.transition().attr("stroke-width", 2);
-                this.parentElement.appendChild(this);
             })
             .on("mouseout", function(d, i) {
-                var sel = d3.select(this);
-                // Only do mouseout radius functionality if we're not already clicked
-                if (!sel.classed("clickedCircle")) {
-                    sel.attr("r", CIRCLE_RADIUS);
+                if (_this.usemouseover == 'true') {
+                    var sel = d3.select(this);
+                    // Only do mouseout radius functionality if we're not already clicked
+                    if (!sel.classed("clickedCircle")) {
+                        sel.attr("r", _this.circleradius);
+                    }
+                    sel.transition().attr("stroke-width", 1);
                 }
-                sel.transition().attr("stroke-width", 1);
             });
         
         // Add a legend showing service color and corresponding name
-        var legend = vis.append('g')
-            .attr('class', 'legend')
-            .attr('x', 50)
-            .attr('y', 50)
-            .attr('height', 100)
-            .attr('width', 100);
-            
-        var legendBoxWidth = 100;
-        legend.selectAll('g').data(services)
-          .enter()
-          .append('g')
-          .each(function(d, i) {
-            var g = d3.select(this);
-            g.append("rect")
-              .attr("x", i * legendBoxWidth + 5)
-              .attr("y", 10)
-              .attr("width", 10)
-              .attr("height", 10)
-              .attr("stroke", "black")
-              .attr("stroke-width", 0.5)
-              .style("fill", function() {
-                  var toReturn = colors[String(d)];
-                  if (!toReturn) {
-                      return DEFAULT_COLOR;
-                  }
-                  return toReturn;
+        if (this.uselegend == 'true') {
+            var legend = vis.append('g')
+                .attr('class', 'legend')
+                .attr('x', 50)
+                .attr('y', 50)
+                .attr('height', 100)
+                .attr('width', 100);
+                
+            var legendBoxWidth = 100;
+            legend.selectAll('g').data(services)
+              .enter()
+              .append('g')
+              .each(function(d, i) {
+                var g = d3.select(this);
+                g.append("rect")
+                  .attr("x", i * legendBoxWidth + 5)
+                  .attr("y", 10)
+                  .attr("width", 10)
+                  .attr("height", 10)
+                  .attr("stroke", "black")
+                  .attr("stroke-width", 0.5)
+                  .style("fill", function() {
+                      var toReturn = colors[String(d)];
+                      if (!toReturn) {
+                          return _this.defaultcolor;
+                      }
+                      return toReturn;
+                  });
+                
+                g.append("text")
+                  .attr("x", i * legendBoxWidth + 20)
+                  .attr("y", 18)
+                  .attr("height", 10)
+                  .attr("width", legendBoxWidth)
+                  .attr("title", "'" + d + "' service")
+                  .style("fill", "black")
+                  .text(d);
               });
-            
-            g.append("text")
-              .attr("x", i * legendBoxWidth + 20)
-              .attr("y", 18)
-              .attr("height", 10)
-              .attr("width", legendBoxWidth)
-              .attr("title", "'" + d + "' service")
-              .style("fill", "black")
-              .text(d);
-          });
+        }
         
         // Add a very simple resize that duplicates this method with a new width/height
-        d3.select(window).on('resize', function() {
-            _this.show(data);
-        });
+        if (this.useresize == 'true') {
+            d3.select(window).on('resize', function() {
+                _this.show(data);
+            });
+        }
+        
+        // Update our graph title with the proper count
+        this.customTitle = "Graph of " + data.length + " Events:";
     },
     
     //******************PRIVATE API******************
