3dc1d5b3f9cfd29d6bcc187b029d515ac803f22e
cruzmiraback <cmiraback@icesoft.com>
2015-12-03 16:09:22 -0700

    Update

--- components/bridgeit-web-components/bridgeit-action-editor/bridgeit-action-editor.js
+++ components/bridgeit-web-components/bridgeit-action-editor/bridgeit-action-editor.js
@@ -816,10 +816,12 @@ Polymer({
      * @private
      */
     _sortProperties: function(a,b) {
-        a = a.title.toLowerCase();
-        b = b.title.toLowerCase();
-        if (a < b) { return -1; }
-        else if (a > b) { return  1; }
+        if (a && b) {
+            a = a.title.toLowerCase();
+            b = b.title.toLowerCase();
+            if (a < b) { return -1; }
+            else if (a > b) { return  1; }
+        }
         return 0;
     },
 
