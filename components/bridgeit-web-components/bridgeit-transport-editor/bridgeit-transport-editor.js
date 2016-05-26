af5049fb49f5e500f9e36839078051cd03be2f55
cgug <carlo.guglielmin@icesoft.com>
2016-05-26 15:10:34 -0600

    Sync

--- components/bridgeit-web-components/bridgeit-transport-editor/bridgeit-transport-editor.js
+++ components/bridgeit-web-components/bridgeit-transport-editor/bridgeit-transport-editor.js
@@ -3,9 +3,18 @@ Polymer({
 
     properties: {
         /**
-         * Enable debug mode, where the component can be manually submitted and the template JSON viewed
+         * Enable debug mode where the underlying JSON data structure will be displayed on the page for review
          */
         debug: { type: Boolean, value: false, reflectToAttribute: true, notify: true },
+        /**
+         * The current value of the transport editor. Data binding is enabled for this attribute.
+         * This will link to the underlying notification JSON structure
+         */
+        value: { type: Object,  observer: '_valueChanged', reflectToAttribute: true, notify: true },
+        /**
+         * Underlying UI tool control state
+         */
+        _tool: { type: Object, observer: '_toolChanged', notify: true },
         /**
          * Allow the Browser transport to be used
          * If this (or any "allow" flag is false) the user won't be able to interact with that transport
@@ -43,13 +52,23 @@ Polymer({
          * The value of the Email transport checkbox
          * This will be overridden if allowEmail=false
          */        
-        defaultEmail: { type: Boolean, value: false, reflectToAttribute: true, notify: true },
+        defaultEmail: { type: Boolean, value: false, reflectToAttribute: true, notify: true }
     },
+    observers: [
+        '_toolChanged(_tool.transport.browser, _tool.transport.cloud, _tool.transport.sms, _tool.transport.email,' +
+                      '_tool.subject.usebrowser, _tool.subject.usecloud, _tool.subject.usesms, _tool.subject.useemail, _tool.subject.global, _tool.subject.browser, _tool.subject.cloud, _tool.subject.sms, _tool.subject.email,' +
+                      '_tool.details.usebrowser, _tool.details.usecloud, _tool.details.usesms, _tool.details.useemail, _tool.details.global, _tool.details.browser, _tool.details.cloud, _tool.details.sms, _tool.details.email,' +
+                      '_tool.url.usebrowser, _tool.url.usecloud, _tool.url.usesms, _tool.url.useemail, _tool.url.global, _tool.url.browser, _tool.url.cloud, _tool.url.sms, _tool.url.email,' +
+                      '_tool.priority.usebrowser, _tool.priority.usecloud, _tool.priority.usesms, _tool.priority.useemail, _tool.priority.global, _tool.priority.browser, _tool.priority.cloud, _tool.priority.sms, _tool.priority.email,' +
+                      '_tool.expiryTime.global, _tool.payload)'
+    ],
     
     /**
      * Define our initial tool data structure for backing our UI controls
      */
 	ready: function() {
+	    this.triggeredFromTool = false;
+	    
 	    // Log if we don't have any transports available
 	    this.noTransports = false;
 	    if (!this.allowBrowser && !this.allowCloud && !this.allowSMS && !this.allowEmail) {
@@ -57,79 +76,10 @@ Polymer({
 	        this.noTransports = true;
 	    }
 	    
-        this.tool = {
-            "transport": {
-                "browser": this.allowBrowser ? this.defaultBrowser : false,
-                "cloud": this.allowCloud ? this.defaultCloud : false,
-                "sms": this.allowSMS ? this.defaultSMS : false,
-                "email": this.allowEmail ? this.defaultEmail : false
-            },
-            "subject": {
-                "usebrowser": true,
-                "usecloud": true,
-                "usesms": true,
-                "useemail": true,
-                "generic": null,
-                "browser": null,
-                "cloud": null,
-                "sms": null,
-                "email": null
-            },
-            "details": {
-                "usebrowser": true,
-                "usecloud": true,
-                "usesms": true,
-                "useemail": true,
-                "generic": null,
-                "browser": null,
-                "cloud": null,
-                "sms": null,
-                "email": null
-            },
-            "url": {
-                "usebrowser": true,
-                "usecloud": true,
-                "usesms": true,
-                "useemail": true,
-                "generic": null,
-                "browser": null,
-                "cloud": null,
-                "sms": null,
-                "email": null
-            },
-            "expiryDate": {
-                "usebrowser": true,
-                "usecloud": true,
-                "usesms": true,
-                "useemail": true,
-                "generic": null,
-                "browser": null,
-                "cloud": null,
-                "sms": null,
-                "email": null
-            },
-            "priority": {
-                "usebrowser": true,
-                "usecloud": true,
-                "usesms": true,
-                "useemail": true,
-                "generic": null,
-                "browser": null,
-                "cloud": null,
-                "sms": null,
-                "email": null
-            },
-            "payload": ""
-        };
-	},
-	
-	/**
-	 * Function to debug our JSON notification template and allow it to be displayed to the user
-	 * This will also log the data to the web console
-	 */
-	debugSubmit: function() {
-	    this.set('debugJSON', JSON.stringify(this.getTemplateJSON(), null, 4));
-	    console.log(this.debugJSON);
+	    // If we don't have a valid tool state (such as nothing being passed via 'value') we default
+	    if (typeof this._tool === 'undefined' || this._tool === null) {
+	        this._setDefaultTool();
+	    }
 	},
 	
 	/**
@@ -138,84 +88,87 @@ Polymer({
 	 *  proper way to store this in a message template
 	 * This is the main call into this component, as the fields themselves don't need to be interacted with
 	 */
-	getTemplateJSON: function() {
+	convertUIToJSON: function() {
 	    var toReturn = {};
 	    
-	    if (this.allowBrowser && this.tool.transport.browser) {
-	        toReturn.browser = {
-	            "message": {
-	                "payload": this.tool.payload
-	            }
-            };
-            toReturn.browser.message.metadata = this._generateMetadata("browser");
+	    // Payload may be in the process of updating, so ignore any errors for now
+	    this.validPayload = {};
+	    try{
+	        validPayload = JSON.parse(this._tool.payload);
+	    }catch(ignored) { }
+	    
+	    // First add our global JSON
+	    toReturn.global = {
+	        "subject": this._tool.subject.global,
+	        "details": this._tool.details.global,
+	        "url": this._tool.url.global,
+	        "priority": this._tool.priority.global,
+	        "expire_time": this._tool.expiryTime.global,
+	        "sent_time": new Date(),
+	        "payload": validPayload
+	    };
+	    
+	    if (this.allowBrowser && this._tool.transport.browser) {
+	        toReturn.browser = this._getOverrideData("browser");
 	    }
-	    if (this.allowCloud && this.tool.transport.cloud) {
-	        toReturn.cloud = {
-	            "data": { }
-	        };
-	        
-	        if (this._hasField("details", "cloud")) {
-	            toReturn.cloud.details = this._getField("details", "cloud");
-	        }
-	        if (this._hasField("subject", "cloud")) {
-	            toReturn.cloud.subject = this._getField("subject", "cloud");
-	        }
-	        if (this._hasField("url", "cloud")) {
-	            toReturn.cloud.url = this._getField("url", "cloud");
-	        }
-	        
-	        toReturn.cloud.data.metadata = this._generateMetadata("cloud");
+	    if (this.allowCloud && this._tool.transport.cloud) {
+	        toReturn.cloud = this._getOverrideData("cloud");
 	    }
-	    if (this.allowEmail && this.tool.transport.email) {
-	        toReturn.email = {
-	            "data": { }
-	        };
-	        toReturn.email.data.metadata = this._generateMetadata("email");
+	    if (this.allowEmail && this._tool.transport.email) {
+	        toReturn.email = this._getOverrideData("email");
 	    }
-	    if (this.allowSMS && this.tool.transport.sms) {
-	        toReturn.sms = {
-	            "data": { }
-	        };
-	        toReturn.sms.data.metadata = this._generateMetadata("sms");
+	    if (this.allowSMS && this._tool.transport.sms) {
+	        toReturn.sms = this._getOverrideData("sms");
 	    }
 	    
 	    return toReturn;
 	},
 	
 	/**
-	 * Function to generate metadata JSON for the passed transport
+	 * Convert passed JSON notification data to a valid UI control state
+	 * @param json
+	 */
+	convertJSONToUI: function(json) {
+	    // First clear the state of our tool, or populate defaults as necessary
+	    this._setDefaultTool();
+	    
+	    // TODO Next try to convert the incoming JSON to valid UI tool state
+	},
+	
+	/**
+	 * Function to generate override data JSON for the passed transport
 	 * This will pull data from our internal UI controls and populate (if available):
-	 *  desc, url, expiryDate, priority
+	 *  subject, details, url, priority
 	 * @param transport
 	 */
-	_generateMetadata: function(transport) {
-	    var metadata = { };
+	_getOverrideData: function(transport) {
+	    var toReturn = { };
 	    
+	    if (this._hasField("subject", transport)) {
+	        toReturn.subject = this._getField("subject", transport);
+	    }
 	    if (this._hasField("details", transport)) {
-	        metadata.desc = this._getField("details", transport);
+	        toReturn.details = this._getField("details", transport);
 	    }
 	    if (this._hasField("url", transport)) {
-	        metadata.url = this._getField("url", transport);
-	    }
-	    if (this._hasField("expiryDate", transport)) {
-	        metadata.expiryDate = this._getField("expiryDate", transport);
+	        toReturn.url = this._getField("url", transport);
 	    }
 	    if (this._hasField("priority", transport)) {
-	        metadata.priority = this._getField("priority", transport);
+	        toReturn.priority = this._getField("priority", transport);
 	    }
 	    
-	    return metadata;
+	    return toReturn;
 	},
 	
 	/**
 	 * Function to return the value of a single field from our UI controls
 	 * This will determine if the user requested we use the generic global value or a specific override value
-	 * Basically look at the "useTransport" flag and return data for either "generic" or "transport"
+	 * Basically look at the "useTransport" flag and return data for either "global" or "transport"
 	 * @param field such as "details" or "url"
 	 * @param transport such as "browser" or "cloud"
 	 */
 	_getField: function(field, transport) {
-	    return this.tool[field]['use' + transport] ? this.tool[field]['generic'] : this.tool[field][transport];
+	    return this._tool[field]['use' + transport] ? null : this._tool[field][transport];
 	},
 	
 	/**
@@ -227,6 +180,98 @@ Polymer({
 	_hasField: function(field, transport) {
 	    var toCheck = this._getField(field, transport);
 	    
-	    return typeof toCheck !== 'undefined' && toCheck !== null;
-	}
+	    return typeof toCheck !== 'undefined' && toCheck !== null && toCheck != "";
+	},
+	
+	/**
+	 * Function called when the main value for this component changes
+	 */
+    _valueChanged: function() {
+        // If we're triggered from a tool change we just ignore
+        // The only time we want to do anything with our new data is if it's set from a non-tool change
+        // Such as an initial load from an attribute specified on the page
+        var setDebugData = this.value;
+        if (!this.triggeredFromTool) {
+            this.convertJSONToUI(this.value);
+        }
+        else {
+            setDebugData = this.convertUIToJSON();
+        }
+        this.triggeredFromTool = false;
+        
+        // Update our debug panel if visible
+        if (this.debug) {
+            this.set('debugJSON', JSON.stringify(setDebugData, null, 4));
+        }
+    },
+    
+    /**
+     * Function called when the underlying UI tooling changes for this component
+     */
+    _toolChanged: function() {
+        this.triggeredFromTool = true;
+        this.set('value', this.convertUIToJSON());
+    },
+    
+    /**
+     * Set the default state of the UI tooling controls
+     */
+    _setDefaultTool: function() {
+        this.set('_tool', {
+            "transport": {
+                "browser": this.allowBrowser ? this.defaultBrowser : false,
+                "cloud": this.allowCloud ? this.defaultCloud : false,
+                "sms": this.allowSMS ? this.defaultSMS : false,
+                "email": this.allowEmail ? this.defaultEmail : false
+            },
+            "subject": {
+                "usebrowser": true,
+                "usecloud": true,
+                "usesms": true,
+                "useemail": true,
+                "global": null,
+                "browser": null,
+                "cloud": null,
+                "sms": null,
+                "email": null
+            },
+            "details": {
+                "usebrowser": true,
+                "usecloud": true,
+                "usesms": true,
+                "useemail": true,
+                "global": null,
+                "browser": null,
+                "cloud": null,
+                "sms": null,
+                "email": null
+            },
+            "url": {
+                "usebrowser": true,
+                "usecloud": true,
+                "usesms": true,
+                "useemail": true,
+                "global": null,
+                "browser": null,
+                "cloud": null,
+                "sms": null,
+                "email": null
+            },
+            "expiryTime": {
+                "global": 4320,
+            },
+            "priority": {
+                "usebrowser": true,
+                "usecloud": true,
+                "usesms": true,
+                "useemail": true,
+                "global": "info",
+                "browser": null,
+                "cloud": null,
+                "sms": null,
+                "email": null
+            },
+            "payload": "{}"
+        });
+    }
 });
