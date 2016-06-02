19a88c0da8e532bd930ae0fabbe4fa39f0861e12
cruzmiraback <cmiraback@icesoft.com>
2016-06-02 10:49:18 -0600

    action-editor - Use code editor for data properties.

--- bridgeit-action-editor/bridgeit-action-editor.js
+++ bridgeit-action-editor/bridgeit-action-editor.js
@@ -36,11 +36,7 @@ Polymer({
         }
         this._loadedAction = null;
         this._taskGroups = [];
-<<<<<<< Updated upstream
-        this._codeEditorProperties=['function','messagetemplate','transporttemplate','query','payload','userrecord','pushmessage'];
-=======
-        this._codeEditorProperties=['function','messagetemplate','query','payload','userrecord','pushmessage','data'];
->>>>>>> Stashed changes
+        this._codeEditorProperties=['function','messagetemplate','transporttemplate','query','payload','userrecord','pushmessage','data'];
         this._taskGroupBaseId = 'taskGroup';
         this._taskBaseId = 'task';
         
