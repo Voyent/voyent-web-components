f81152efa8cfe2053876ddceea7504ba3055cd52
Carlo G <carlo.guglielmin@icesoft.com>
2017-04-12 12:23:31 -0600

    Added an approach to Wizard for attaching validation to any step, to prevent the Next button from moving panels

--- voyent-wizard-panel/voyent-wizard-panel.js
+++ voyent-wizard-panel/voyent-wizard-panel.js
@@ -88,6 +88,7 @@ Polymer({
     
     ready: function() {
         this.useDynamicAnimation = (this.entryanimation === "dynamic");
+        this.validators = [];
         
         // Timeout set the default `selected`
         // This is so that any parent pages using the component can add their event listener in time
@@ -97,7 +98,7 @@ Polymer({
             _this.selected = 0;
         },0);
         
-        if (this.getChildCount() <= 0) {
+        if (this._getChildCount() <= 0) {
             console.error("No children pages found for voyent-wizard-panel");
         }
     },
@@ -109,21 +110,47 @@ Polymer({
     selectedChanged: function(selected) {
         // Disable Back if we don't have a previous page, and similarly Next if we are at the end
         this.previousdisabled = (selected-1 < 0);
-        this.nextdisabled = (selected+1 >= this.getChildCount());
+        this.nextdisabled = (selected+1 >= this._getChildCount());
         
         this.fire('voyent-wizard-panel-changed', { selected: this.selected });
+    },
+    
+    /**
+     * Attach a validation function to a certain selected index
+     * The passed function needs to return a boolean true/false
+     * The step is not zero based, but relates to the currently selected pane of the wizard
+     * Whenever Next is clicked any attached validator on that step will be fired, and if it returns false
+     *  the Next panel switching will be aborted
+     *
+     * @param step non-zero index to attach to
+     * @param parent context to use in the function call, should be "this" of the parent
+     * @param validatorFn validation function that returns true/false
+     */
+    attachValidator: function(step, parent, validatorFn) {
+         // Account for zero index by doing step-1
+        this.validators[step-1] = { "parent": parent, "fn": fn };
     },
     
     /**
      * Increment the selected page number.
      */
     next: function() {
-        if (this.useDynamicAnimation) {
-            this.entryanimation = "slide-from-right-animation";        
+        // Check for any attached validators
+        var valid = true;
+        if (this.validators[this.selected]) {
+            // Call the underlying validation function, and pass in the parent "this"
+            valid = this.validators[this.selected].fn.call(this.validators[this.selected].parent);
         }
-        this.selected++;
         
-        this.fire('voyent-wizard-panel-next', { selected: this.selected });
+        // Only continue processing if we're valid
+        if (valid) {
+            if (this.useDynamicAnimation) {
+                this.entryanimation = "slide-from-right-animation";        
+            }
+            this.selected++;
+            
+            this.fire('voyent-wizard-panel-next', { selected: this.selected });
+        }
     },
     
     /**
@@ -138,7 +165,7 @@ Polymer({
         this.fire('voyent-wizard-panel-previous', { selected: this.selected });
     },
     
-    getChildCount: function() {
+    _getChildCount: function() {
         return this.querySelector('#pages').childElementCount;
     },
 });
