f784034597483976fd4f9de77fa9e307ebd56763
Carlo G <carlo.guglielmin@icesoft.com>
2017-08-29 12:47:34 -0600

    Added a new 'voyent-toggle-panel' component that is an expandable/collapsible card with plenty of customization

---
+++ voyent-toggle-panel/voyent-toggle-panel.js
@@ -0,0 +1,28 @@
+Polymer({
+    is: 'voyent-toggle-panel',
+
+    properties: {
+        visible: {
+            type: Boolean,
+            value: false
+        },
+        expanded: {
+            type: Boolean,
+            value: false
+        },
+        header: {
+            type: String
+        },
+    },
+    
+    attached: function() {
+        var _this = this;
+        setTimeout(function() {
+            _this.visible = true;
+        },200);
+    },
+    
+    toggle: function(e) {
+        this.set('expanded', !this.expanded);
+    },
+});
