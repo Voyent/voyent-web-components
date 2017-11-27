06133a631aac6bf03593d237efe1a5262daa6881
Carlo G <carlo.guglielmin@icesoft.com>
2017-11-27 14:49:33 -0700

    Added an attribute to the voyent-loading component to lock it to the desired state and not accept any listener events. Handy if you have a wrapper that doesn't the hiding and you just want the voyent-loading comp to work more like a dedicated throbber

--- voyent-loading/voyent-loading.js
+++ voyent-loading/voyent-loading.js
@@ -3,16 +3,19 @@ Polymer({
 
     properties: {
         on: { type: Boolean, value: false, notify: true, reflectToAttribute: true },
+        locked: { type: Boolean, value: false, notify: true, reflectToAttribute: true }
     },
     
     ready: function() {
-        var _this = this;
-        window.addEventListener('loading-on',  function(e) {
-            _this.on = true;
-        });
-        
-        window.addEventListener('loading-off',  function(e) {
-            _this.on = false;
-        });
+        if (!this.locked) {
+            var _this = this;
+            window.addEventListener('loading-on',  function(e) {
+                _this.on = true;
+            });
+            
+            window.addEventListener('loading-off',  function(e) {
+                _this.on = false;
+            });
+        }
     },
 });
