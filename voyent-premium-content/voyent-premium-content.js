495f5b2daa123bd53b1f045ad8a18a37df2d0778
Carlo G <carlo.guglielmin@icesoft.com>
2018-06-06 15:49:46 -0600

    Cleaned up premium content to actually check our account record

--- voyent-premium-content/voyent-premium-content.js
+++ voyent-premium-content/voyent-premium-content.js
@@ -4,11 +4,12 @@ Polymer({
     properties: {
         hasPremium: {
             type: Boolean,
-            value: true,
+            value: false,
             observer: '_premiumChanged'
         },
         noPremium: {
-            type: Boolean
+            type: Boolean,
+            value: false
         },
         bubblePos: {
             type: String,
@@ -25,23 +26,24 @@ Polymer({
     },
     
     ready: function() {
-        var _this = this;
-        window.addEventListener('account-level-changed', function(e) {
-            if (e && e.detail && typeof e.detail.premium !== 'undefined') {
-                _this.set('hasPremium', e.detail.premium);
-            }
-        });
-        
-        var accountLevel = voyent.$.getSessionStorageItem(btoa("accountLevel"));
-        if (typeof accountLevel === 'string') {
-            this.set('hasPremium', accountLevel === 'true' ? true : false);
-        }
-        
         // Override if given the right flag
         // Necessary because specifying a boolean attribute, even with a value of false, is still interpreted by Polymer as true
         // So saying has-premium="false" on the parent page sets true
         if (this.noPremium === true) {
             this.set('hasPremium', false);
+        }
+        else {
+            var _this = this;
+            voyent.admin.getAccount().then(function(res) {
+                if (res && res.serviceLevels && res.serviceLevels.accountType &&
+                    typeof res.serviceLevels.accountType.code !== 'undefined') {
+                        if (res.serviceLevels.accountType.code === 2) { // Premium
+                            _this.set('hasPremium', true);
+                        }
+                    }
+            }).catch(function(error) {
+                console.error(error);
+            });
         }
     },
     
