3503afb8aa58a9f07e82e0e27b096db280c03706
cmiraback <cmiraback@icesoft.com>
2019-03-21 15:05:12 -0600

    VRAS-829 - Mobile Admin: Alerts not respecting templates with saved locations

--- voyent-mobile-alert-editor/voyent-mobile-alert-editor.js
+++ voyent-mobile-alert-editor/voyent-mobile-alert-editor.js
@@ -37,25 +37,26 @@ Polymer({
                 coordinates = {"lat":center.lat(),"lng":center.lng()};
             }
         }
-        //Clear the map of any loaded alert template before drawing.
+        // Clear the map of any loaded alert template before drawing
         if (this._loadedAlert) {
             this.clearMap();
         }
-        //Remove the parent's id from the record as we'll generate a new one.
+        // Remove the parent's id from the record as we'll generate a new one
         var id = template._id;
         delete template._id;
-        //If we have a geometry then use the provided location as the alert center.
+        // If we have a geometry and this is not a fixed location
+        // template then use the provided location as the alert center
         var latLng = null;
-        if (template.geo) {
+        if (template.geo && !template.properties.center) {
             latLng = new google.maps.LatLng(coordinates);
         }
         template.state = 'draft'; //Default to draft
-        //Set this flag so the center_changed listener will not fire for each circular zone that is drawn.
+        // Set this flag so the center_changed listener will not fire for each circular zone that is drawn
         this._ignoreZoneCenterChangedEvent = true;
         this._drawAndLoadAlertTemplate(template,latLng);
         this._loadedAlert.template.setParentId(id);
         this._setIsAlertLoading(false);
-        //Populate the movement pane, async so the properties panel has time to initialize.
+        // Populate the movement pane, async so the properties panel has time to initialize
         setTimeout(function() {
             _this._ignoreZoneCenterChangedEvent = false;
         },0);
